9f4e8e3009bed0d637e921eedf1688d9f0458dba
Dhriti Burugupalli <burugupallidhriti@gmail.com>
2025-07-25 09:58:40 -0700

    Initial Commit

---
+++ Birthday/BirthdayApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdayApp.swift
+//  Birthday
+//
+//  Created by Scholar on 7/25/25.
+//
+
+import SwiftUI
+
+@main
+struct BirthdayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
